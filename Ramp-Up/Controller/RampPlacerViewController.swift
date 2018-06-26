c887a1c78ea4a0fe20a8564939d07c2e35b8da0c
Henry Chukwu <henry.chukwu@andela.com>
2018-06-26 11:39:58 +0100

    project setup

--- Ramp-Up/Controller/RampPlacerViewController.swift
+++ Ramp-Up/Controller/RampPlacerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RampPlacerViewController.swift
 //  Ramp-Up
 //
 //  Created by Henry Chukwu on 6/26/18.
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class RampPlacerViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
